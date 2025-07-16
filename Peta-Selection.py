09c544193bbb1be2c5961b4ede5a93a8942b08ee
Arifqi <ahpla999@gmail.com>
2025-07-16 08:10:36 +0700

    Update Peta-Selection.py

--- Peta-Selection.py
+++ Peta-Selection.py
@@ -93,7 +93,7 @@ def create_clustered_map(df, selected_station_type='AAWS'):
                 <b>Province:</b> {site['nama_propinsi']}<br>
                 <b>District:</b> {site['nama_kota']}<br>
                 <b>Coordinates:</b> {site['latt_station']:.3f}, {site['long_station']:.3f}<br>
-                <b>Elevation:</b> {site['elv_station']:.3f} m <br>
+                <b>Elevation:</b> {f"{site['elv_station']:.3f}" if pd.notna(site['elv_station']) else 'N/A'} m <br>
                 <b>Installation Year:</b> {site['tgl_pasang'].strftime("%m/%d/%Y") if pd.notna(site['tgl_pasang']) else 'N/A'}<br>
                 <b>Equipment:</b> {site['nama_vendor'] if pd.notna(site['nama_vendor']) else 'N/A'}<br>
                 <b>Address:</b> {site['addr_instansi'] if pd.notna(site['addr_instansi']) else 'N/A'}
@@ -163,7 +163,7 @@ def create_selective_map(df, selected_station_type='AAWS', selected_id_station='
             <b>Province:</b> {site.get('nama_propinsi', 'N/A')}<br>
             <b>District:</b> {site.get('nama_kota', 'N/A')}<br>
             <b>Coordinates:</b> {site['latt_station']:.3f}, {site['long_station']:.3f}<br>
-            <b>Elevation:</b> {site['elv_station']:.3f} m <br>
+            <b>Elevation:</b> {f"{site['elv_station']:.3f}" if pd.notna(site['elv_station']) else 'N/A'} m <br>
             <b>Installation Year:</b> {install_date}<br>
             <b>Equipment:</b> {site.get('nama_vendor', 'N/A')}<br>
             <b>Address:</b> {site.get('addr_instansi', 'N/A')}
